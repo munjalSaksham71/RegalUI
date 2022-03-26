0e7b0fe9439bb1268268b6179fb776412e710bec
munjalSaksham71 <sakshammunjal4@gmail.com>
2022-03-26 23:29:14 +0530

    Refactor: Using Event Listener for click

--- docs/docs.js
+++ docs/docs.js
@@ -1,15 +1,18 @@
 const snackbar = document.getElementById("snackbar");
 const modal = document.getElementById("modal");
+const show_modal = document.getElementById("show_modal");
+const close_modal = document.getElementById("close_modal")
 
 const showSnackbar = () => {
     snackbar.className = "show";
     setTimeout(() => { snackbar.className = snackbar.className.replace("show", ""); }, 3000);
 }
 
-const openModal = () => {
+show_modal.addEventListener("click", () => {
     modal.style.visibility = "visible"
-}
+})
 
-const closemodel = () => {
+close_modal.addEventListener("click", () => {
     modal.style.visibility = "hidden"
-}
+})
+
