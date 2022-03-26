4dec49602eae6e9ae802a8c5101746b099c2152f
munjalSaksham71 <sakshammunjal4@gmail.com>
2022-03-26 23:03:18 +0530

    Feat: Added Modal Component

--- docs/docs.js
+++ docs/docs.js
@@ -1,6 +1,15 @@
 const snackbar = document.getElementById("snackbar");
+const modal = document.getElementById("modal");
 
 const showSnackbar = () => {
     snackbar.className = "show";
     setTimeout(() => { snackbar.className = snackbar.className.replace("show", ""); }, 3000);
+}
+
+const openModal = () => {
+    modal.style.visibility = "visible"
+}
+
+const closemodel = () => {
+    modal.style.visibility = "hidden"
 }
